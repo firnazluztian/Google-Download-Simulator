1f7de9f0183ee3ac54e360f3a76ce1460f8c3ee7
firnaz <firnazluztian@gmail.com>
2015-10-22 13:11:06 -0400

    Initial commit

---
+++ app/src/main/java/firnazluztian/addon/com/googledownloadsimulator/backup.java
@@ -0,0 +1,145 @@
+package firnazluztian.addon.com.googledownloadsimulator;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class backup extends Activity {
+
+    public static final int DIALOG_DOWNLOAD_PROGRESS = 1;
+    private Button startBtn, endBtn;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.startBtn);
+        startBtn.setText("Download");
+        startBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startBtn.setText("Downloading.....");
+                startDownload();
+            }
+        });
+
+        endBtn = (Button)findViewById(R.id.endBtn);
+        endBtn.setText("Uninstall");
+        endBtn.setVisibility(View.INVISIBLE);
+    }
+
+    private void startDownload() {
+        String url = "http://www.hdwallpapers.in/walls/crysis_hd_1080p-HD.jpg";
+        new DownloadFileAsync().execute(url);
+    }
+
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        switch (id) {
+            case DIALOG_DOWNLOAD_PROGRESS:
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setMessage("Downloading file...");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                //progressDialog.setCancelable(false);
+                progressDialog.show();
+                return progressDialog;
+            default:
+                return null;
+        }
+    }
+
+    public void onClickPage2(View view) {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        startActivity(intent);
+    }
+
+    class DownloadFileAsync extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            int count;
+
+            try {
+
+                URL url = new URL(params[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                int lengthofFile = connection.getContentLength();
+                Log.d("ANDRO_ASYNC", "Length of file: " + lengthofFile);
+
+                InputStream input = new BufferedInputStream(url.openStream());
+                OutputStream output = new FileOutputStream("sdcard/downloaded_photo.jpg");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    publishProgress("" + (int) ((total * 100) / lengthofFile));
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+            dismissDialog(DIALOG_DOWNLOAD_PROGRESS);
+
+            endBtn.setVisibility(View.VISIBLE);
+
+            startBtn.setText("Open");
+            //startBtn.setBackgroundColor(Color.parseColor("#85ef85"));
+            startBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // do nothing
+                }
+            });
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            showDialog(DIALOG_DOWNLOAD_PROGRESS);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            // TODO Auto-generated method stub
+            Log.d("ANDRO_ASYNC", values[0]);
+            progressDialog.setProgress(Integer.parseInt(values[0]));
+        }
+    }
+}
